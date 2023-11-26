7c0d4dd75f369106c8274baf46aa31837a95080d
marinvukelja <149719176+marinvukelja@users.noreply.github.com>
2023-11-26 15:09:56 +0100

    Add files via upload
    3. Zadatak

---
+++ zad3.asm
@@ -0,0 +1,20 @@
+@MAIN
+0; JMP
+
+(MAIN)
+        D = 0;
+        @0
+        D = M;
+        @1
+        D = D + M;
+        @2
+        D = D + M;        
+        @3
+        D = D + M;        
+        @4
+        D = D + M;
+        @5
+        M = D;
+        (MAIN_END)
+        @MAIN_END
+        0; JMP
